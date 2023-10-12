9b3c95fc94d258aa8e549b46d1ffaa601dc7d820
Mike Jefferson Vizcarra <145326653+emm-jee@users.noreply.github.com>
2023-10-13 00:24:02 +0800

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,13 +1,1 @@
-fetch('/postPrint', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-    }
-})
-.then(response => response.json())
-.then(data => {
-    console.log(data);
-})
-.catch(error => {
-    console.error('Error:', error);
-});
+
